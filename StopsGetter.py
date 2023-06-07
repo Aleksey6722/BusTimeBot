b635f553817cbe7f602b282a89414bba26ce991d
Minaev V <11@>
2023-06-07 16:54:17 +0600

    tram stops getter

--- StopsGetter.py
+++ StopsGetter.py
@@ -3,17 +3,19 @@ import requests
 
 
 def get_stops():
-    a_dict = {'Point1': {'Latitude': 49.935127, 'Longitude': 82.666678}, 'Point2': {'Latitude': 49.89085, 'Longitude': 82.718598}}
+    a_dict = {'Point1': {'Latitude': 50.007318, 'Longitude': 82.55763}, 'Point2': {'Latitude': 49.947647, 'Longitude':  82.643141}}
     resp = requests.post('https://oskemenbus.kz/api/GetStops', json=a_dict)
-    t = resp.text
-    l = ''
-    c = 0
+    l = []
     for i in resp.text.split('\n')[:-1]:
         result = json.loads(i)
         name = result.get('result').get('StopName')
         id = result.get('result').get('StopId')
-        l += id + ', ' + name + '\n'
-        c += 1
+        resp1 = requests.post('https://oskemenbus.kz/api/GetScoreboard', json={"StopId": id})
+        st = resp1.text.split('\n')[0]
+        obj = json.loads(st)
+        stop_type = obj.get('result').get('Type')
+        if stop_type == 2:
+            l.append((name, id, stop_type))
     return l
 
 st = get_stops()
